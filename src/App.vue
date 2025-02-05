b3ea8ba24adf45389c1dba702d9c2f90139b0694
Noel Kampus <noel@kampus.ch>
2025-02-05 19:05:04 +0100

    bug: deleted

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <template>
-  <Analytics />
+  <!-- <Analytics /> -->
   <router-view />
 </template>
 
-<script setup>
-import { Analytics } from '@vercel/analytics/vue';
-
+<script>
+// import { Analytics } from '@vercel/analytics/vue';
 export default {
   name: "App"
 };
