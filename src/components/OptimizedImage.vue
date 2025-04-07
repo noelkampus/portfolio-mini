bb2f7b9e67be37a1b78333b6a7ba64c06f24c1b2
Noel Kampus <noel@kampus.ch>
2025-04-07 20:32:23 +0200

    feat: optimized images

---
+++ src/components/OptimizedImage.vue
@@ -0,0 +1,18 @@
+<template>
+  <img :src="optimizedSrc" loading="lazy" decoding="async" v-bind="$attrs" />
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  src: { type: String, required: true },
+  width: { type: Number, default: 800 },
+  quality: { type: Number, default: 75 },
+})
+
+const optimizedSrc = computed(() => {
+  if (!props.src) return ''
+  return `/_vercel/image?url=${encodeURIComponent(props.src)}&w=${props.width}&q=${props.quality}`
+})
+</script>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import OptimizedImage from './components/OptimizedImage.vue'
+
+const app = createApp(App)
+
+app.component('OptimizedImage', OptimizedImage) // <-- this replaces ALL <img> tags globally
 
 createApp(App).use(router).mount('#app')
